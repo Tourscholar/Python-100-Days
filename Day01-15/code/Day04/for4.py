3973db7e5898f7018e585c9d4190609e21fae010
Tourscholar <40328997+Tourscholar@users.noreply.github.com>
2019-11-19 00:39:02 +0900

    master

--- Day01-15/code/Day04/for4.py
+++ Day01-15/code/Day04/for4.py
@@ -10,11 +10,13 @@ from math import sqrt   #sqrt(x)函数返回数字x的平方根
 num = int(input('请输入一个正整数: '))
 end = int(sqrt(num))
 is_prime = True
+if num == 1:
+    is_prime = False
 for x in range(2, end + 1):
     if num % x == 0:
         is_prime = False
         break
-if is_prime and num != 1:
+if is_prime == True:
     print('%d是素数' % num)
 else:
     print('%d不是素数' % num)
